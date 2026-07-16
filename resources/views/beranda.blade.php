4cbf25bf4e40b4a895de8fcdb54f9a806f9aefac
Afterfive_06 <ahmadnajihalmatsany038@gmail.com>
2026-07-16 10:05:14 +0700

    tailwind

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -3,38 +3,21 @@
 @section('judul', 'Beranda')
 
 @section('konten')
+    <h1>Selamat Datang di Toko ABC</h1>
+    <p>Produk unggulan kami.</p>
 
-<h1>Selamat Datang di Toko ABC</h1>
-<p>Produk unggulan kami.</p>
-
-<div class="row">
-
-    @foreach($produkUnggulan as $produk)
-
-        <div class="col-md-3 mb-3">
-            <div class="card">
-                <div class="card-body">
-
-                    <h5>{{ $produk['nama'] }}</h5>
-
-                    <p>
-                        {{ $produk['deskripsi'] }}
-                    </p>
-
-                    <p>
-                        Rp {{ number_format($produk['harga'], 0, ',', '.') }}
-                    </p>
-
-                    <p>
-                        Stok: {{ $produk['stok'] }}
-                    </p>
-
+    <div class="row">
+        @foreach($produkUnggulan as $produk)
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>{{ $produk['nama'] }}</h5>
+                        <p>
+                            Rp {{ number_format($produk['harga'], 0, ',', '.') }}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-
-    @endforeach
-
-</div>
-
+        @endforeach
+    </div>
 @endsection
